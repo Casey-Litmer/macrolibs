20ce1784f12a7eb7b506a6a7cea064862e5b52a9
Casey Litmer <litmerc@msn.com>
2024-11-08 02:54:35 -0800

    Added misc submodule with timer wrapper

---
+++ macrolibs/misc/misc.py
@@ -0,0 +1,18 @@
+from time import perf_counter
+
+
+def timer(print_time = True):
+    def time_function(f):
+        def wrapper(*args, **kwargs):
+            t0 = perf_counter()
+            result = f(*args, **kwargs)
+            time = perf_counter() - t0
+
+            if print_time:
+                print(time)
+                return result
+            else:
+                return (result, time)
+
+        return wrapper
+    return time_function
